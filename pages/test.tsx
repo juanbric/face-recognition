2a4971175a9b70fbafbcb154cded98f49ab69a0a
Juan Briceno <juanbri@gmx.com>
2023-01-10 16:09:32 +0100

    was able to pull data from storage

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,39 +1,26 @@
-
-import { storage } from "../config/firebase";
-import {
-  ref,
-  listAll,
-  getDownloadURL,
-  list,
-} from "firebase/storage";
-import firebase from "firebase/app";
-import "firebase/storage";
 import MetaTag from "../components/MetaTag";
-import { useEffect, useState } from "react";
-
+import { getStorage, ref, getMetadata } from "firebase/storage";
+import { storage } from "../config/firebase"
 
 const test = () => {
-    const [imageUrls, setImageUrls] = useState([]);
-    // const imagesListRef = ref(storage, "images/");
-    // useEffect(() => {
-    //     listAll(imagesListRef).then((response) => {
-    //       response.items.forEach((item) => {
-    //         getDownloadURL(item).then((url) => {
-    //           setImageUrls((prev) => [...prev, url]);
-    //         });
-    //       });
-    //     });
-    //   }, []);
-
+    const forestRef = ref(storage, 'Black-Widow/1.jpg');
+    
+    getMetadata(forestRef)
+    .then((metadata) => {
+      console.log(metadata)
+    })
+    .catch((error) => {
+        console.log(error)
+    });
   return (
     <div className="py-16 flex flex-col items-center justify-center">
-    <MetaTag
-      title={"Reconoce"}
-      description={"Aplicación de detección y ¨Rreconocimiento facial"}
-    />
-    <p>Hola</p>
-  </div>
-  )
-}
+      <MetaTag
+        title={"Reconoce"}
+        description={"Aplicación de detección y ¨Rreconocimiento facial"}
+      />
+      <p>Hola</p>
+    </div>
+  );
+};
 
-export default test
+export default test;

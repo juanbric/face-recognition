54a075a5960b9a94566e3a32bf5b697aa00e4b44
Juan Briceno <juanbri@gmx.com>
2023-01-12 19:43:27 +0100

    squares on faces

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Layout>
   );
 }
-
-

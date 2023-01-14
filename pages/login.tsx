ee02010081a3459ce0c74f2929ff407fb2cce4ae
Juan Briceno <juanbri@gmx.com>
2023-01-14 23:47:53 +0100

    auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,13 +4,15 @@ import router from "next/router";
 import MetaTag from "../components/MetaTag";
 import { auth, firestore, provider } from "../config/firebase";
 
+const whiteList = ["juan@soltype.io", "jpbd94@gmail.com"]
+
 const Login = () => {
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     const logIn = onAuthStateChanged(auth, (user) => {
       if (user) {
         const docuRef = doc(firestore, `usuarios/${user?.uid}`);
-        setDoc(docuRef, { correo: user?.email, rol: "user" });
+        setDoc(docuRef, { correo: user?.email, rol: whiteList.includes(user?.email) ? "admin" : "user"});
         console.log("user data sent to Firestore", user?.email);
         router.replace("/home");
       }

0ac28930cd705d881fe73dc0272c3a4c9b860ddf
Juan Briceno <juanbri@gmx.com>
2023-01-15 17:34:55 +0100

    tags

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -3,26 +3,52 @@ import React, { useEffect, useState } from "react";
 import { ref, uploadBytes } from "firebase/storage";
 //@ts-ignore
 import { v4 } from "uuid";
+import { SimpleModal } from "./SimpleModal";
+import Link from "next/link";
 
-const Upload = ({ imageUpload, faceMatches }: { imageUpload: any, faceMatches: any }) => {
+const Upload = ({
+  imageUpload,
+  faceMatches,
+  grado,
+  fecha,
+}: {
+  imageUpload: any;
+  faceMatches: any;
+  grado: any;
+  fecha: any;
+}) => {
+  const [submit, setSubmit] = useState(false);
+
   const uploadImage = () => {
     if (imageUpload == null) return;
-    
-    {console.log("facematches", faceMatches)}
     //@ts-ignore
-    const imageRef = ref(storage, `images/${' ' + faceMatches + v4()}`);
+    const imageRef = ref(
+      storage,
+      `images/${" " + faceMatches + grado + " " + fecha + " " + v4()}`
+    );
     uploadBytes(imageRef, imageUpload).then(() => {
-      alert("Image Uploaded");
+      setSubmit(true);
     });
   };
 
   return (
-    <button
-      className="text-sm py-2 mb-4 px-4 rounded-full border-0 font-semibold bg-blue-50 text-blue-700 mt-8 hover:bg-blue-100"
-      onClick={uploadImage}
-    >
-      Upload image
-    </button>
+    <>
+      <button
+        className="text-sm py-2 mb-4 px-4 rounded-full border-0 font-semibold bg-blue-50 text-blue-700 mt-8 hover:bg-blue-100"
+        onClick={uploadImage}
+      >
+        Upload image
+      </button>
+
+      <SimpleModal
+        isOpen={submit}
+        onClose={() => {
+          setSubmit(false);
+        }}
+        headerText={"¡Éxito!"}
+        description={`Tu imágen se ha subido exitosamente con sus tags y reconocimientos correspondientes.`}
+      />
+    </>
   );
 };
 
